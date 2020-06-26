66ab96cae0239ab33792e40229acc1e63a0750a7
takutakumi <ryuryutakumi@gmail.com>
2020-06-26 16:40:19 +0900

    start

---
+++ System Configuration Map/test1.py
@@ -0,0 +1,7 @@
+from diagrams import Diagram
+from diagrams.k8s.clusterconfig import HPA
+from diagrams.k8s.compute import Deployment, Pod, ReplicaSet
+from diagrams.k8s.network import Ingress, Service
+
+with Diagram("Kubernetes Pod", show=False):
+    Pod("pod1")
